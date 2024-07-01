08103aac59357d877ad7a59fed91743a300685f6
abe-josh <123541858+abe-josh@users.noreply.github.com>
2024-07-02 01:24:01 +0800

    Task 07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,31 @@
 const convertToCelsius = function() {
+  let fahrenheit = arguments[0];
+
+  let result = (fahrenheit - 32) * (5/9);
+
+  let celcius = Number(result.toFixed(1));
+
+  console.log("Result : " , result);
+  console.log("Celcius : " , celcius);
+
+  return celcius;
+
+
 };
 
 const convertToFahrenheit = function() {
+
+  let celcius = arguments[0];
+
+  let result = celcius * (9/5) + 32;
+
+  let fahrenheit = Number(result.toFixed(1));
+
+  console.log("Result : " , result);
+  console.log("Fahrenheit : " , fahrenheit);
+
+  return fahrenheit;
+
 };
 
 // Do not edit below this line

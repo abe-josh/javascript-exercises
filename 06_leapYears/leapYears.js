bf9f93d6ca53bacaec7804f59a96cc599be42b08
abe-josh <123541858+abe-josh@users.noreply.github.com>
2024-07-02 00:22:47 +0800

    Task 06 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
 const leapYears = function() {
+    let year = arguments[0];
 
+    if(!(year % 400 === 0)) {
+        let result = year % 100;
+
+        if((result % 4 === 0) && (result > 0)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return true;
+    }
+
+    
 };
 
 // Do not edit below this line
